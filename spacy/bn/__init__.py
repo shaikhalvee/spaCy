29fd05f626986cf223d9b55dde5ad5519ed78594
Aniruddha Adhikary <aniruddha@adhikary.net>
2017-02-21 00:14:10 +0600

    initially add Bengali support

---
+++ spacy/bn/__init__.py
@@ -0,0 +1,16 @@
+# encoding: utf8
+from __future__ import unicode_literals, print_function
+
+from ..language import Language
+from ..attrs import LANG
+from .language_data import *
+
+
+class Bengali(Language):
+    lang = 'bn'
+
+    class Defaults(Language.Defaults):
+        lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
+        lex_attr_getters[LANG] = lambda text: 'bn'
+
+        stop_words = STOP_WORDS
